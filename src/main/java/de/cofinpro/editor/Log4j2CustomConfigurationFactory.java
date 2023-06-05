f1c5f318e63ebebeae5b579b6e3a35b4a0254c12
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-06-05 09:37:47 +0200

    Remove filters from log4j-config

--- src/main/java/de/cofinpro/editor/Log4j2CustomConfigurationFactory.java
+++ src/main/java/de/cofinpro/editor/Log4j2CustomConfigurationFactory.java
@@ -1,7 +1,6 @@
 package de.cofinpro.editor;
 
 import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.ConsoleAppender;
 import org.apache.logging.log4j.core.config.Configuration;
@@ -24,14 +23,10 @@ public class Log4j2CustomConfigurationFactory extends ConfigurationFactory {
     static Configuration createConfiguration(final String name, ConfigurationBuilder<BuiltConfiguration> builder) {
         builder.setConfigurationName(name);
         builder.setStatusLevel(Level.INFO);
-        builder.add(builder.newFilter("ThresholdFilter", Filter.Result.ACCEPT, Filter.Result.NEUTRAL).
-                addAttribute("level", Level.DEBUG));
         AppenderComponentBuilder appenderBuilder = builder.newAppender(STDOUT, "CONSOLE").
                 addAttribute("target", ConsoleAppender.Target.SYSTEM_OUT);
         appenderBuilder.add(builder.newLayout("PatternLayout").
                 addAttribute("pattern", "%d{dd.MM.yyyy HH:mm:ss} - %p - %C{1}.%M L%L: %m%n"));
-        appenderBuilder.add(builder.newFilter("MarkerFilter", Filter.Result.DENY,
-                Filter.Result.NEUTRAL).addAttribute("marker", "FLOW"));
         builder.add(appenderBuilder);
         builder.add(builder.newLogger("org.apache.logging.log4j", Level.DEBUG).
                 add(builder.newAppenderRef(STDOUT)).
